0ae34c34a01323441f46937cf9e4fb3100863a40
Mazen Ramadan <mazenramadan00@gmail.com>
2022-11-30 00:12:09 +0200

    Add customer model for customer service

---
+++ customer/src/database/models/Customer.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const CustomerSchema = new Schema({
+    email: String,
+    password: String,
+    salt: String,
+    phone: String,
+    address:[
+        { type: Schema.Types.ObjectId, ref: 'address', require: true }
+    ],
+
+    // Define cart instance
+    cart: [
+        {
+            product: {
+                _id: { type: String, require: true},
+                name: { type: String},
+                banner: { type: String},
+                price: { type: Number}
+            },
+            unit: { type: Number, require: true}
+        }
+    ],
+
+    // Dfine whishlist instance
+    wishlist:[
+        { 
+            _id: { type: String, require: true},
+            name: { type: String},
+            description: {type: String},
+            banner: { type: String},
+            available: {type: Boolean},
+            price: { type: Number}
+        }
+    ],
+
+    // Define order instance 
+    orders: [ 
+        { 
+            _id: { type: String, require: true},
+            amount: { type: String},
+            date: {type: Date, default: Date.now()}
+
+        }
+    ]
+},{
+    toJSON: {
+        transform(doc, ret){
+            delete ret.password;
+            delete ret.salt;
+            delete ret.__v;
+        }
+    },
+    timestamps: true
+});
+
+module.exports =  mongoose.model('customer', CustomerSchema);

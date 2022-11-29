0a3866337bb843ba1f14a3e650252149272a82f2
Mazen Ramadan <mazenramadan00@gmail.com>
2022-11-29 23:37:55 +0200

    Add connection to MongoDB for each service

---
+++ customer/src/database/connection.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const  DB_URL  = require('../config');
+
+module.exports = async() => {
+    
+    try {
+        await mongoose.connect(DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connedted to MongoDB successfully');
+        
+    } catch (error) {
+        console.log('Error happened')
+        console.log(error);
+        process.exit(1);
+    };
+};

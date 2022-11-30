8865eeec0e436e32641bb05b0f45532f61347e3e
Mazen Ramadan <mazenramadan00@gmail.com>
2022-11-30 15:06:25 +0200

    Add repository to handle data operations

---
+++ customer/src/database/repository/customer-repository.js
@@ -0,0 +1,4 @@
+const {CustomerModel, AddressModel} = require('../models');
+const { APIError, BadRequestError, STATUS_CODES } = require('../../utils/app-errors');
+
+// Dealing with data operations

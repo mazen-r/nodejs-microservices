2baa2b19a6b4e7e8b2415c97a9888b1c5db67385
Mazen Ramadan <mazenramadan00@gmail.com>
2022-11-30 13:57:04 +0200

    Add authentication middleware

---
+++ customer/src/api/middleware/middleware.js
@@ -0,0 +1,11 @@
+const ValidateSignature = require('../../utils');
+
+module.exports = async (req, res, next) => {
+    const isAuthorized = await ValidateSignature(req);
+
+    if (isAuthorized) {
+        return next();
+    } else {
+        return res.status(403).json({message: 'You are not Authorized'});
+    };
+};

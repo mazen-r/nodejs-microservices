e28e46bf863cb0bacb5a7dcc53ca905665d227cb
Mazen Ramadan <mazenramadan00@gmail.com>
2022-12-01 23:59:44 +0200

    Add create customer controller

--- customer/src/database/repository/customer-repository.js
+++ customer/src/database/repository/customer-repository.js
@@ -2,3 +2,21 @@ const {CustomerModel, AddressModel} = require('../models');
 const { APIError, BadRequestError, STATUS_CODES } = require('../../utils/app-errors');
 
 // Dealing with data operations
+class CustomerRepository {
+    async CreateCustomer ({ email, password, phone, salt}) {
+        try {
+            const customer = new CustomerModel ({
+                email,
+                password,
+                salt,
+                phone,
+                address: []
+            })
+            const customerResult = await customer.save()
+            return customerResult;
+        } catch (error) {
+            throw APIError('API Error', STATUS_CODES.INTERNAL_ERROR, 'Unable to create customer')
+        }
+    };
+
+}

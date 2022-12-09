5b657baf8c447e1f489da2c34e1f37a1cff94443
Mazen Ramadan <mazenramadan00@gmail.com>
2022-12-09 23:10:44 +0200

    Add listener for customer service

---
+++ customer/src/api/app-events.js
@@ -0,0 +1,12 @@
+const CustomerService = require('../controllers/customer-service');
+
+module.exports = (app) => {
+    const service = new CustomerService()
+
+    app.use('/app-events', async (req, res, next) => {
+        const {payload} = req.body
+        service.SubscribeEvents(payload)
+        console.log("===============  Customer Service Received Event ====== ")
+        res.status(200).json(payload)
+    });
+};

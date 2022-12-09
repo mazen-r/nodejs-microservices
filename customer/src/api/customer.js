8d21537eee56deb4b4474b0ddd4b04bf69a697a7
Mazen Ramadan <mazenramadan00@gmail.com>
2022-12-09 23:11:35 +0200

    Add signup route

---
+++ customer/src/api/customer.js
@@ -0,0 +1,18 @@
+const CustomerService = require('../controllers/customer-service');
+const UserAuth = require('./middleware/auth');
+
+module.exports = (app) => {
+    const service = new CustomerService()
+
+    app.post('/signup', async (req, res, next) => {
+        try {
+            const {email, password, phone} = req.body
+            const {data} = await service.SignUp({ email, password, phone})
+            return res.json(data);
+        } catch (err) {
+            next(err)
+        }
+    });
+
+
+};

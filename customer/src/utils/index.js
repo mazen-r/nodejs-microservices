fd15ae20b06299649a077a159d898bbb4f6cebe7
Mazen Ramadan <mazenramadan00@gmail.com>
2023-01-09 18:03:29 +0200

    Add rabbitmq utils

--- customer/src/utils/index.js
+++ customer/src/utils/index.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const amqplib = require('amqplib');
 
-const { APP_SECRET } = require('../config');
+const { APP_SECRET, EXCHANGE_NAME, CUSTOMER_SERVICE } = require('../config');
 
 // Util functions
 
@@ -40,4 +41,41 @@ module.exports.FormateData = (data) => {
     } else {
         throw new Error('Data not found!')
     };
+};
+
+//Message Broker
+module.exports.CreateChannel = async() => {
+    try {
+            const connection = await amqplib.connect('amqp://localhost');
+            const channel = await connection.createChannel();
+            await channel.assertQueue(EXCHANGE_NAME, 'direct' ,{ durable: true});
+            return channel
+    } catch (err) {
+            throw err
+    }
+};
+
+module.exports.PublishMessage = (channel,service,msg, ) => {
+    channel.publish(EXCHANGE_NAME, service, Buffer.from(msg));
+    console.log('Sent: ', msg);
+};
+
+module.exports.SubscribeMessage = async (channel, service) => {
+  
+    await channel.assertExchange(EXCHANGE_NAME, 'direct' ,{ durable: true});
+    const q =  await channel.assertQueue('', { exclusive: true });
+    console.log(` Waiting for messages in queue: ${q.queue}`);
+
+    channel.bindQueue(q.queue, EXCHANGE_NAME, CUSTOMER_SERVICE);
+
+    channel.consume( q.queue, msg => {
+
+    if(msg.content){
+            console.log("the message is:", msg.content.toString());
+            service.SubscribeEvents(msg.content.toString())
+    }
+            console.log("[X] received")
+    }, {
+            noAck: true
+    });
 };

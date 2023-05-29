53ec6b04d91798886ca583909b7cddc62d1c2c4f
Mazen Ramadan <mazenramadan00@gmail.com>
2023-05-29 15:17:19 +0300

    feat: Error handling auth middleware

--- customer/src/api/middleware/auth.js
+++ customer/src/api/middleware/auth.js
@@ -1,11 +1,14 @@
 const { ValidateSignature } = require('../../utils');
 
 module.exports = async (req, res, next) => {
-    const isAuthorized = await ValidateSignature(req);
-
-    if (isAuthorized) {
-        return next();
-    } else {
-        return res.status(403).json({message: 'You are not Authorized'});
+    try {
+        const isAuthorized = await ValidateSignature(req);
+        if (isAuthorized) {
+            return next();
+        } else {
+            return res.status(403).json({message: 'You are not Authorized'});
+        };
+    } catch (err) {
+        return res.status(400).json({message: 'invalid token'});
     };
 };
--- product/src/api/middlewares/auth.js
+++ product/src/api/middlewares/auth.js
@@ -1,11 +1,13 @@
 const { ValidateSignature } = require('../../utils');
 
 module.exports = async (req,res,next) => {
-    
-    const isAuthorized = await ValidateSignature(req);
-
-    if(isAuthorized){
-        return next();
-    }
-    return res.status(403).json({message: 'Not Authorized'})
+    try {
+        const isAuthorized = await ValidateSignature(req);
+        if(isAuthorized){
+            return next();
+        }
+        return res.status(403).json({message: 'Not Authorized'})
+    } catch (err) {
+        return res.status(400).json({message: 'Invalid token'})
+    };
 };
